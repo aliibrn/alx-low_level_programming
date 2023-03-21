1edde9453769c60739fe1282a4e3abaefd1107c8
aliibrn <alibrne53@gmail.com>
2023-03-21 14:58:34 -0700

    workhard

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,7 +15,7 @@ void print_to_98(int n)
 
 	if (n > 98)
 		for (num = n; num > 98; --num)
-			printf("%d, ", count);
+			printf("%d, ", num);
 	else
 		for (num = n; num < 98; ++numt)
 			printf("%d, ", num);
